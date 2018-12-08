a7233ec1957ba4bea2481f2c5c75f977cd8b9db4
Styphix <altenyt@gmail.com>
2018-12-07 21:01:24 -0500

    Last change, i hope...

--- GDEdit/GDE.App/Program.cs
+++ GDEdit/GDE.App/Program.cs
@@ -6,7 +6,7 @@ namespace GDE.App
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             Game app = new GDEApp();
             DesktopGameHost host = Host.GetSuitableHost("GDE");
