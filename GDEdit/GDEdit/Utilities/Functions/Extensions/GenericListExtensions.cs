c974ab3daad1676fb7bd67115fa07727e014673d
AlFas <AlFasGD@users.noreply.github.com>
2018-12-21 14:10:36 +0200

    Split extensions into more files

---
+++ GDEdit/GDEdit/Utilities/Functions/Extensions/GenericListExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Functions.Extensions
+{
+    /// <summary>Provides generic extension methods for lists.</summary>
+    public static class GenericListExtensions
+    {
+        #region Cloning
+        public static List<T> Clone<T>(this List<T> l)
+        {
+            List<T> result = new List<T>();
+            for (int i = 0; i < l.Count; i++)
+                result.Add(l[i]);
+            return result;
+        }
+        public static List<List<T>> Clone<T>(this List<List<T>> l)
+        {
+            List<List<T>> result = new List<List<T>>();
+            for (int i = 0; i < l.Count; i++)
+            {
+                result.Add(new List<T>());
+                for (int j = 0; j < l[i].Count; j++)
+                    result[i].Add(l[i][j]);
+            }
+            return result;
+        }
+        #endregion
+
+        #region Contain Checks
+        public static bool ContainsAll<T>(this List<T> list, List<T> containedList)
+        {
+            if (containedList.Count != list.Count)
+                return false;
+            List<T> tempList = list.Clone();
+            List<T> tempContained = containedList.Clone();
+            for (int i = 0; i < tempContained.Count; i++)
+                tempList.Remove(tempContained[i]);
+            return tempList.Count == 0;
+        }
+        public static bool ContainsUnordered<T>(this List<T> list, List<T> containedList)
+        {
+            List<T> tempList = containedList.Clone();
+            List<T> tempContained = containedList.Clone();
+            for (int i = 0; i < tempList.Count; i++)
+                tempList.Remove(tempContained[i]);
+            return list.Count - containedList.Count == tempList.Count;
+        }
+        #endregion
+
+        #region Intradimensional
+        public static int[] GetLengths<T>(this List<T[]> l)
+        {
+            int[] lengths = new int[l.Count];
+            for (int i = 0; i < l.Count; i++)
+                lengths[i] = l[i].Length;
+            return lengths;
+        }
+        public static T[,] ToTwoDimensionalArray<T>(this List<T[]> l)
+        {
+            T[,] ar = new T[l.Count, l.GetLengths().Max()];
+            for (int i = 0; i < l.Count; i++)
+                for (int j = 0; j < l[i].Length; j++)
+                    ar[i, j] = l[i][j];
+            return ar;
+        }
+        #endregion
+    }
+}
---
+++ GDEdit/GDEdit/Utilities/Functions/Extensions/IntArrayExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Functions.Extensions
+{
+    /// <summary>Provides generic extension methods for <seealso cref="int"/> arrays.</summary>S
+    public static class IntArrayExtensions
+    {
+        public static string ShowValuesWithRanges(this int[] s)
+        {
+            s = s.Sort(); // Sort the values
+            string result = "";
+            if (s.Length > 0)
+            {
+                int lastShownValue = s[0];
+                int lastValueInCombo = s[0];
+                result += lastShownValue.ToString();
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] > lastValueInCombo) // Determines whether the next value is not in a row
+                    {
+                        result += (result[result.Length - 1] == '-' ? s[i - 1].ToString() : "") + ", " + s[i].ToString();
+                        lastValueInCombo = lastShownValue = s[i] + 1;
+                    }
+                    else if (s[i] == lastValueInCombo) // Determines whether the next value is in a row
+                    {
+                        if (i < s.Length - 1) // Determines whether the current index is not the last value
+                        {
+                            if (lastShownValue == lastValueInCombo) // Determines whether this is the start of a new combo
+                                result += "-";
+                        }
+                        else if (i == s.Length - 1) // Determines whether the current index is the last value
+                            result += s[i].ToString();
+                        lastValueInCombo = s[i] + 1; // Set the last index in the combo to the current index
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
