19cfebce02488d37b6c5bbdac4f785b33874f627
AlFas <AlFasGD@users.noreply.github.com>
2018-12-23 21:15:32 +0200

    Add Toggle trigger

---
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/Triggers/ToggleTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GDEdit.Utilities.Attributes;
+using GDEdit.Utilities.Enumerations.GeometryDash.GamesaveValues;
+using GDEdit.Utilities.Objects.GeometryDash.LevelObjects.Triggers.Interfaces;
+
+namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.Triggers
+{
+    /// <summary>Represents a Toggle trigger.</summary>
+    public class ToggleTrigger : Trigger, IHasTargetGroupID
+    {
+        public override int ObjectID => (int)Enumerations.GeometryDash.Trigger.Toggle;
+        
+        /// <summary>The target Group ID of the trigger.</summary>
+        public int TargetGroupID { get; set; }
+        /// <summary>The Activate Group property of the trigger.</summary>
+        [ObjectStringMappable(ObjectParameter.ActivateGroup)]
+        public bool ActivateGroup { get; set; }
+
+        /// <summary>Initializes a new instance of the <seealso cref="ToggleTrigger"/> class.</summary>
+        public ToggleTrigger() { }
+        /// <summary>Initializes a new instance of the <seealso cref="ToggleTrigger"/> class.</summary>
+        /// <param name="targetGroupID">The target Group ID of the trigger.</param>
+        /// <param name="activateGroup">The Activate Group property of the trigger.</summary>
+        public ToggleTrigger(int targetGroupID, bool activateGroup = false)
+        {
+            TargetGroupID = targetGroupID;
+            ActivateGroup = activateGroup;
+        }
+
+        // TODO: Add cloning method
+    }
+}
