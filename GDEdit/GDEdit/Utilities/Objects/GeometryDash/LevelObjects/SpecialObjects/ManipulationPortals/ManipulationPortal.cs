67caa2ae7247a5411cb5b394ee25ef9883093841
AlFas <AlFasGD@users.noreply.github.com>
2018-12-29 13:08:15 +0200

    Improve manipulation portal's implementation

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/ManipulationPortals/ManipulationPortal.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/ManipulationPortals/ManipulationPortal.cs
@@ -8,15 +8,15 @@ using System.Threading.Tasks;
 namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.SpecialObjects.ManipulationPortals
 {
     /// <summary>Represents a manipulation portal (a portal that changes the player's gamemode).</summary>
-    public abstract class ManipulationPortal
+    public abstract class ManipulationPortal : SpecialObject
     {
-        /// <summary>The gamemode the manipulation portal transforms the player tinto.</summary>
+        /// <summary>The object ID of the manipulation portal.</summary>
+        public new abstract int ObjectID { get; }
+
+        /// <summary>The gamemode the manipulation portal transforms the player into.</summary>
         public abstract Gamemode Gamemode { get; }
 
         /// <summary>Initializes a new instance of the <seealso cref="ManipulationPortal"/> class.</summary>
-        public ManipulationPortal()
-        {
-
-        }
+        public ManipulationPortal() : base() { }
     }
 }
