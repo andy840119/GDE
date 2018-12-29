88f9cb0db70e31a8b55b4ebba6c9f1efde163ab2
AlFas <AlFasGD@users.noreply.github.com>
2018-12-29 13:02:35 +0200

    Generalize abstraction

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/RotatingObject.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/RotatingObject.cs
@@ -12,19 +12,18 @@ namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.SpecialObjects
     /// <summary>Represents a rotating object with rotating properties.</summary>
     public class RotatingObject : SpecialObject
     {
+        protected override int[] ValidObjectIDs => ObjectLists.RotatingObjectList;
+        protected override string SpecialObjectType => "rotating object";
+
         /// <summary>Represents the Disable Rotation property of the rotating object.</summary>
         [ObjectStringMappable(ObjectParameter.DisableRotation)]
         public bool DisableRotation;
 
         /// <summary>Initializes a new instance of the <seealso cref="RotatingObject"/> class.</summary>
         /// <param name="objectID">The object ID of the rotating object.</param>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
+        /// <param name="x">The X location of the object.</param>
+        /// <param name="y">The Y location of the object.</param>
         public RotatingObject(int objectID, double x, double y)
-            : base(objectID, x, y)
-        {
-            if (!ObjectLists.RotatingObjectList.Contains(objectID))
-                throw new InvalidCastException("This object ID does not represent a valid rotating object.");
-        }
+            : base(objectID, x, y) { }
     }
 }
--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/SpecialObject.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/SpecialObject.cs
@@ -9,6 +9,10 @@ namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.SpecialObjects
     /// <summary>Represents a special object.</summary>
     public abstract class SpecialObject : GeneralObject
     {
+        /// <summary>The valid object IDs of the special object. Only override if the class does not represent a single object.</summary>
+        protected virtual int[] ValidObjectIDs => null;
+        protected virtual string SpecialObjectType => "special object";
+
         /// <summary>Initializes a new instance of the <seealso cref="SpecialObject"/> class.</summary>
         public SpecialObject() { }
         /// <summary>Initializes a new instance of the <seealso cref="SpecialObject"/> class.</summary>
@@ -16,6 +20,11 @@ namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.SpecialObjects
         /// <param name="x">The X location of the object.</param>
         /// <param name="y">The Y location of the object.</param>
         public SpecialObject(int objectID, double x, double y)
-            : base(objectID, x, y) { }
+            : base(objectID, x, y)
+        {
+            // Since this constructor must be only used by general object classes that may have different object IDs, assume the property is overriden
+            if (!ValidObjectIDs.Contains(objectID))
+                throw new InvalidCastException($"This object ID does not represent a valid {SpecialObjectType}.");
+        }
     }
 }
