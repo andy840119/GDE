b983bc05c52aa5878979c4c3e04a93ab558df595
AlFas <AlFasGD@users.noreply.github.com>
2018-12-29 12:56:03 +0200

    Add rotating object class

---
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/RotatingObject.cs
@@ -0,0 +1,30 @@
+﻿using GDEdit.Utilities.Attributes;
+using GDEdit.Utilities.Enumerations.GeometryDash.GamesaveValues;
+using GDEdit.Utilities.Information.GeometryDash;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.SpecialObjects
+{
+    /// <summary>Represents a rotating object with rotating properties.</summary>
+    public class RotatingObject : SpecialObject
+    {
+        /// <summary>Represents the Disable Rotation property of the rotating object.</summary>
+        [ObjectStringMappable(ObjectParameter.DisableRotation)]
+        public bool DisableRotation;
+
+        /// <summary>Initializes a new instance of the <seealso cref="RotatingObject"/> class.</summary>
+        /// <param name="objectID">The object ID of the rotating object.</param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public RotatingObject(int objectID, double x, double y)
+            : base(objectID, x, y)
+        {
+            if (!ObjectLists.RotatingObjectList.Contains(objectID))
+                throw new InvalidCastException("This object ID does not represent a valid rotating object.");
+        }
+    }
+}
--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/SpecialObject.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/SpecialObject.cs
@@ -7,12 +7,15 @@ using System.Threading.Tasks;
 namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.SpecialObjects
 {
     /// <summary>Represents a special object.</summary>
-    public abstract class SpecialObject
+    public abstract class SpecialObject : GeneralObject
     {
         /// <summary>Initializes a new instance of the <seealso cref="SpecialObject"/> class.</summary>
-        public SpecialObject()
-        {
-
-        }
+        public SpecialObject() { }
+        /// <summary>Initializes a new instance of the <seealso cref="SpecialObject"/> class.</summary>
+        /// <param name="objectID">The object ID of the rotating object.</param>
+        /// <param name="x">The X location of the object.</param>
+        /// <param name="y">The Y location of the object.</param>
+        public SpecialObject(int objectID, double x, double y)
+            : base(objectID, x, y) { }
     }
 }
