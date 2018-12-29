b836e2ec953276ad9ccd7a3c5a60769f4644980d
AlFas <AlFasGD@users.noreply.github.com>
2018-12-29 17:46:16 +0200

    Rename pickup item

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/PickupItem.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/PickupItem.cs
@@ -10,33 +10,33 @@ using System.Threading.Tasks;
 
 namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.SpecialObjects
 {
-    /// <summary>Represents a pickup item object.</summary>
-    public class PickupItemObject : SpecialObject, IHasTargetGroupID
+    /// <summary>Represents a pickup item.</summary>
+    public class PickupItem : SpecialObject, IHasTargetGroupID
     {
         protected override int[] ValidObjectIDs => ObjectLists.PickupItemList;
-        protected override string SpecialObjectType => "pickup item object";
+        protected override string SpecialObjectType => "pickup item";
 
-        /// <summary>Represents the Pickup Mode property of the pickup item object.</summary>
+        /// <summary>Represents the Pickup Mode property of the pickup item.</summary>
         [ObjectStringMappable(ObjectParameter.PickupMode)]
         public PickupItemPickupMode PickupMode { get; set; }
-        /// <summary>Represents the Count property of the pickup item object.</summary>
+        /// <summary>Represents the Count property of the pickup item.</summary>
         [ObjectStringMappable(ObjectParameter.Count)]
         public int Count { get; set; }
-        /// <summary>Represents the Subtract Count property of the pickup item object.</summary>
+        /// <summary>Represents the Subtract Count property of the pickup item.</summary>
         [ObjectStringMappable(ObjectParameter.SubtractCount)]
         public bool SubtractCount { get; set; }
-        /// <summary>Represents the Target Group ID property of the pickup item object.</summary>
+        /// <summary>Represents the Target Group ID property of the pickup item.</summary>
         [ObjectStringMappable(ObjectParameter.TargetGroupID)]
         public int TargetGroupID { get; set; }
-        /// <summary>Represents the Activate Group property of the pickup item object.</summary>
+        /// <summary>Represents the Enable Group property of the pickup item.</summary>
         [ObjectStringMappable(ObjectParameter.ActivateGroup)]
         public bool EnableGroup { get; set; }
 
-        /// <summary>Initializes a new instance of the <seealso cref="PickupItemObject"/> class.</summary>
-        /// <param name="objectID">The object ID of the pickup item object.</param>
+        /// <summary>Initializes a new instance of the <seealso cref="PickupItem"/> class.</summary>
+        /// <param name="objectID">The object ID of the pickup item.</param>
         /// <param name="x">The X location of the object.</param>
         /// <param name="y">The Y location of the object.</param>
-        public PickupItemObject(int objectID, double x, double y)
+        public PickupItem(int objectID, double x, double y)
             : base(objectID, x, y) { }
     }
 }
