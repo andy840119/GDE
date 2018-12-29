222fb8b28bd2505fd51f7817eaba30cd1cbe6e70
AlFas <AlFasGD@users.noreply.github.com>
2018-12-29 12:47:25 +0200

    Add manipulation portal class

---
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/ManipulationPortals/ManipulationPortal.cs
@@ -0,0 +1,22 @@
+﻿using GDEdit.Utilities.Functions.GeometryDash;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.SpecialObjects.ManipulationPortals
+{
+    /// <summary>Represents a manipulation portal (a portal that changes the player's gamemode).</summary>
+    public abstract class ManipulationPortal
+    {
+        /// <summary>The gamemode the manipulation portal transforms the player tinto.</summary>
+        public abstract Gamemode Gamemode { get; }
+
+        /// <summary>Initializes a new instance of the <seealso cref="ManipulationPortal"/> class.</summary>
+        public ManipulationPortal()
+        {
+
+        }
+    }
+}
