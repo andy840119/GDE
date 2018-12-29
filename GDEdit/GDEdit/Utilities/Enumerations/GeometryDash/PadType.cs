3629234ea31e35f2e5061ab5388f73befd21f2d6
AlFas <AlFasGD@users.noreply.github.com>
2018-12-29 19:33:55 +0200

    Add pad info and classes

---
+++ GDEdit/GDEdit/Utilities/Enumerations/GeometryDash/PadType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Enumerations.GeometryDash
+{
+    /// <summary>Contains the object IDs of the pads.</summary>
+    public enum PadType
+    {
+        // TODO: Add the pads' IDs
+        /// <summary>The yellow pad.</summary>
+        YellowPad = -1,
+        /// <summary>The magenta pad.</summary>
+        MagentaPad = -1,
+        /// <summary>The red pad.</summary>
+        RedPad = -1,
+        /// <summary>The blue pad.</summary>
+        BluePad = -1,
+    }
+}
