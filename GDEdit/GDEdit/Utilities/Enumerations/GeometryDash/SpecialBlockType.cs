84257b4061d3021f314d8a9fb19148b2dfe2f4b4
AlFas <AlFasGD@users.noreply.github.com>
2018-12-30 21:46:38 +0200

    Add special blocks

---
+++ GDEdit/GDEdit/Utilities/Enumerations/GeometryDash/SpecialBlockType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Enumerations.GeometryDash
+{
+    /// <summary>Provides the object IDs of the special blocks.</summary>
+    public enum SpecialBlockType
+    {
+        // TODO: Retrieve the object IDs of these objects
+        /// <summary>The object ID of the D special block.</summary>
+        D = -1,
+        /// <summary>The object ID of the J special block.</summary>
+        J = -1,
+        /// <summary>The object ID of the S special block.</summary>
+        S = -1,
+        /// <summary>The object ID of the H special block.</summary>
+        H = -1,
+    }
+}
