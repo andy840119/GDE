87db00f2a6b832973f0f4010a55b6f67af2d0bdf
AlFas <AlFasGD@users.noreply.github.com>
2018-12-30 21:26:30 +0200

    Add count text object

---
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/CountTextObject.cs
@@ -0,0 +1,25 @@
+﻿using GDEdit.Utilities.Objects.GeometryDash.LevelObjects.Triggers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.SpecialObjects
+{
+    /// <summary>Represents a count text object.</summary>
+    public class CountTextObject : SpecialObject, IHasPrimaryItemID
+    {
+        /// <summary>The Item ID the count text object displays.</summary>
+        public int PrimaryItemID { get; set; }
+        /// <summary>The Item ID the count text object displays.</summary>
+        public int ItemID
+        {
+            get => PrimaryItemID;
+            set => PrimaryItemID = value;
+        }
+
+        /// <summary>Initializes a new instance of the <seealso cref="CountTextObject"/> class.</summary>
+        public CountTextObject() : base() { }
+    }
+}
