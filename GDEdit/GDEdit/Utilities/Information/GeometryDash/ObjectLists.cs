3c784711b2d6f9fe77714afac958044fd756ddf0
AlFas <AlFasGD@users.noreply.github.com>
2018-12-31 15:13:15 +0200

    Prefer reflection over hardcoding

--- GDEdit/GDEdit/Utilities/Information/GeometryDash/ObjectLists.cs
+++ GDEdit/GDEdit/Utilities/Information/GeometryDash/ObjectLists.cs
@@ -28,7 +28,16 @@ namespace GDEdit.Utilities.Information.GeometryDash
         public static readonly int[] AnimatedObjectList = { 919, 920, 921, 923, 924, 1050, 1051, 1052, 1053, 1054, 1516, 1517, 1518, 1519, 1582, 1583, 1586, 1591, 1592, 1593, 1618, 1697, 1698, 1699, 1700, 1849, 1850, 1851, 1852, 1853, 1854, 1855, 1856, 1857, 1858, 1860 };
         /// <summary>The object IDs of all the rotating objects.</summary>
         public static readonly int[] RotatingObjectList = { 85, 86, 87, 97, 137, 138, 139, 154, 155, 156, 180, 181, 182, 183, 184, 185, 186, 187, 188, 222, 223, 224, 375, 376, 377, 378, 394, 395, 396, 678, 679, 680, 740, 741, 742, 997, 998, 999, 1000, 1019, 1020, 1021, 1055, 1056, 1057, 1058, 1059, 1060, 1061, 1521, 1522, 1523, 1524, 1525, 1526, 1527, 1528, 1582, 1619, 1620, 1705, 1706, 1707, 1708, 1709, 1710, 1734, 1735, 1736, 1752, 1831, 1832, 1833, 1834 };
-        /// <summary>The object IDs of all the objects that are not just general objects but cannot be categorized.</summary>
-        public static readonly int[] NotGeneralObjectList = { 13, 29, 30, 32, 33, 36, 47, 84, 85, 86, 87, 97, 105, 111, 137, 138, 139, 141, 154, 155, 156, 180, 181, 182, 183, 184, 185, 186, 187, 188, 200, 201, 202, 203, 222, 223, 224, 375, 376, 377, 378, 394, 395, 396, 678, 679, 680, 740, 741, 742, 744, 899, 900, 901, 915, 997, 998, 999, 1000, 1006, 1007, 1019, 1020, 1021, 1022, 1049, 1055, 1056, 1057, 1058, 1059, 1060, 1061, 1268, 1275, 1330, 1331, 1333, 1334, 1346, 1347, 1520, 1521, 1522, 1523, 1524, 1525, 1526, 1527, 1528, 1582, 1585, 1587, 1589, 1595, 1598, 1611, 1612, 1613, 1614, 1616, 1619, 1620, 1704, 1705, 1706, 1707, 1708, 1709, 1710, 1734, 1735, 1736, 1751, 1752, 1811, 1812, 1814, 1815, 1817, 1818, 1819, 1831, 1832, 1833, 1834, 1839, 1840, 1841, 1842 };
+
+        /// <summary>The object IDs of all the objects that are not general objects (includes all the other categories).</summary>
+        public static int[] GetNotGeneralObjectList()
+        {
+            var fields = typeof(ObjectLists).GetFields().Where(f => f.FieldType == typeof(int[]) && f.IsStatic);
+            List<int> objects = new List<int>();
+            foreach (var p in fields)
+                objects.AddRange((int[])p.GetRawConstantValue());
+            objects.Sort();
+            return objects.ToArray();
+        }
     }
 }
