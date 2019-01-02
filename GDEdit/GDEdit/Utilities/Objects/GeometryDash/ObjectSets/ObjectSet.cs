08cc413bef5ec26b73fc9bd2ce9baa75ff0b34b9
AlFas <AlFasGD@users.noreply.github.com>
2019-01-02 21:58:00 +0200

    Add base object set implementation

---
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/ObjectSets/ObjectSet.cs
@@ -0,0 +1,69 @@
+﻿using GDEdit.Utilities.Objects.GeometryDash.LevelObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Objects.GeometryDash.ObjectSets
+{
+    /// <summary>Represents an object set, which consists of a number of objects for specific purposes.</summary>
+    public class ObjectSet
+    {
+        // TODO: Implement this to support neighboring objects
+
+        /// <summary>The dictionary containing the objects mapped per the points in the <seealso cref="ObjectSet"/>.</summary>
+        public Dictionary<SquarePoints, List<GeneralObject>> Objects { get; set; }
+
+        /// <summary>Initializes a new instance of the <seealso cref="ObjectSet"/> class.</summary>
+        public ObjectSet() { }
+        /// <summary>Initializes a new instance of the <seealso cref="ObjectSet"/> class.</summary>
+        /// <param name="objects">The dictionary containing the set of objects.</param>
+        public ObjectSet(Dictionary<SquarePoints, List<GeneralObject>> objects)
+        {
+            Objects = objects;
+        }
+
+        /// <summary>Gets or sets the list of general objects in the object set.</summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public List<GeneralObject> this[SquarePoints s]
+        {
+            get => Objects[s];
+            set => Objects[s] = value;
+        }
+    }
+    
+    /// <summary>Represents the points of a square.</summary>
+    public enum SquarePoints
+    {
+        /// <summary>No points.</summary>
+        None = 0,
+
+        /// <summary>The top left point.</summary>
+        TopLeft = 1,
+        /// <summary>The top right point.</summary>
+        TopRight = 1 << 1,
+        /// <summary>The bottom left point.</summary>
+        BottomLeft = 1 << 2,
+        /// <summary>The bottom right point.</summary>
+        BottomRight = 1 << 3,
+
+        /// <summary>The top side.</summary>
+        TopSide = TopLeft | TopRight,
+        /// <summary>The left side.</summary>
+        LeftSide = TopLeft | BottomLeft,
+        /// <summary>The bottom side.</summary>
+        BottomSide = BottomLeft | BottomRight,
+        /// <summary>The right side.</summary>
+        RightSide = TopRight | BottomRight,
+
+        /// <summary>The vertical sides (left and right).</summary>
+        VerticalSides = LeftSide | RightSide,
+        /// <summary>The horizontal sides (top and bottom).</summary>
+        HorizontalSides = TopSide | BottomSide,
+
+        /// <summary>All the points.</summary>
+        All = VerticalSides | HorizontalSides,
+    }
+}
