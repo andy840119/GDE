ec916b81b24c923efe5a43682b46a9400233362b
AlFas <AlFasGD@users.noreply.github.com>
2019-01-02 13:12:02 +0200

    Add class for outline creation

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/EditorHelper/EdgeCreation.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/EditorHelper/EdgeCreation.cs
@@ -11,7 +11,7 @@ namespace GDEdit.Utilities.Objects.GeometryDash.EditorHelper
     /// <summary>Represents an object which contains information about the objects to create from an original object that are the edge decoration in the object set.</summary>
     public class EdgeCreation
     {
-        /// <summary>The object ID of the original object which will be used to create the base decoration object from.</summary>
+        /// <summary>The object ID of the original object which will be used to create the edge decoration object from.</summary>
         public int OriginalObjectID { get; set; }
 
         /// <summary>The object IDs of the created object.</summary>
---
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/EditorHelper/OutlineCreation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Objects.GeometryDash.EditorHelper
+{
+    // TODO: Find a better naming for these
+    /// <summary>Represents an object which contains information about the objects to create from an original object that are the outline decoration in the object set.</summary>
+    public class OutlineCreation
+    {
+        /// <summary>The object ID of the original object which will be used to create the outline decoration object from.</summary>
+        public int OriginalObjectID { get; set; }
+
+        /// <summary>The object ID of the created object.</summary>
+        public int CreatedObjectID { get; set; }
+
+        /// <summary>Initializes a new instance of the <seealso cref="OutlineCreation"/> class.</summary>
+        /// <param name="originalObjectID">The original object ID.</param>
+        /// <param name="createdObjectID">The created object ID.</param>
+        public OutlineCreation(int originalObjectID, int createdObjectID)
+        {
+            OriginalObjectID = originalObjectID;
+            CreatedObjectID = createdObjectID;
+        }
+    }
+}
