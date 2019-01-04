b0fb2a8d5ba5c6ef9061568c1de790a045112ea1
AlFas <AlFasGD@users.noreply.github.com>
2019-01-04 21:08:58 +0200

    Change interface's properties to save space 2

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/Triggers/Interfaces/IHasPrimaryBlockID.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/Triggers/Interfaces/IHasPrimaryBlockID.cs
@@ -13,6 +13,6 @@ namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.Triggers.Interfaces
     {
         /// <summary>The primary Block ID of the trigger.</summary>
         [ObjectStringMappable(ObjectParameter.BlockAID)]
-        int PrimaryBlockID { get; set; }
+        short PrimaryBlockID { get; set; }
     }
 }
