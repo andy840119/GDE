422cb309173c0c093d794d7abacd6b7f86de7fe5
AlFas <AlFasGD@users.noreply.github.com>
2019-01-05 23:17:07 +0200

    Add reference

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/TextObject.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/TextObject.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GDEdit.Utilities.Objects.General;
 
 namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.SpecialObjects
 {
