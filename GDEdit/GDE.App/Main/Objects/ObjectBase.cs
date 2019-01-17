3a009f125393fdcc7c782c68504579986d0e4036
Styphix <altenyt@gmail.com>
2019-01-16 20:46:57 -0500

    Drawable Objects

---
+++ GDEdit/GDE.App/Main/Objects/ObjectBase.cs
@@ -0,0 +1,71 @@
+﻿using GDEdit.Utilities.Information.GeometryDash;
+using GDEdit.Utilities.Objects.GeometryDash;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.Graphics;
+using osuTK;
+using System;
+
+namespace GDE.App.Main.Objects
+{
+    // Change this into a Drawable instead of a Container in a later commit
+    ///<summary>Displays an object of any <see cref="ObjectLists"/></summary>
+    public class ObjectBase : Container
+    {
+        private LevelObject lvlObj;
+        private Box Object;
+
+        #region Level Object Variables
+        ///<summary>Tells what Object ID it is. you can switch this id for other object</summary>
+        public int ID;
+        ///<summary>Position X of the object</summary>
+        public double x;
+        ///<summary>Position Y of the object</summary>
+        public double y;
+        ///<summary>Weither it is flipped horizontally</summary>
+        public bool flippedHorizontally;
+        ///<summary>Weither it is flipped horizontally</summary>
+        public bool flippedVertically;
+        ///<summary>Rotation of the object</summary>
+        public double rotation;
+        ///<summary>Scale of the object</summary>
+        public double scale;
+        ///<summary>Editor layer 1 of the object</summary>
+        public int EL1;
+        ///<summary>Editor layer 2 of the object</summary>
+        public int EL2;
+        ///<summary>What and how many groups are in the object</summary>
+        public int[] groupID;
+        #endregion
+
+        public ObjectBase()
+        {
+            lvlObj = new LevelObject(ID, x, y);
+
+            Children = new Drawable[]
+            {
+                Object = new Box
+                {
+                    Size = new Vector2(100),
+                    Origin = Anchor.Centre,
+                    Anchor = Anchor.Centre,
+                }
+            };
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(TextureStore texStore)
+        {
+            switch(ID)
+            {
+                case 1:
+                    Console.WriteLine(Object.Texture = texStore.Get("Objects/square_01_001.png"));
+                    break;
+                default:
+                    throw new Exception("Rest hasnt been done yet");
+            }
+        }
+    }
+}
---
+++ GDEdit/GDE.Tests/Visual/TestCaseObject.cs
@@ -0,0 +1,30 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Testing;
+using osu.Framework.Graphics.Sprites;
+using System.ComponentModel;
+using GDE.App.Main.Screens.Edit;
+using static GDEdit.Utilities.Functions.GeometryDash.Gamesave;
+using GDEdit.Application;
+using GDE.App.Main.Objects;
+
+namespace GDE.Tests.Visual
+{
+    public class TestCaseObject : TestCase
+    {
+        private ObjectBase Obj;
+
+        public TestCaseObject()
+        {
+            Children = new Drawable[]
+            {
+                Obj = new ObjectBase
+                {
+                    Origin = Anchor.Centre,
+                    Anchor = Anchor.Centre,
+                    ID = 1,
+                    Size = new osuTK.Vector2(100)
+                }
+            };
+        }
+    }
+}
