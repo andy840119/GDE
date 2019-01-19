52d644be193fcb252373bd466742bcbda239ab16
Styphix <altenyt@gmail.com>
2019-01-19 14:03:14 -0500

    LevelOverview initialization + fixing the stupid System.Drawing bug

---
+++ GDEdit/GDE.App/Main/lvl/LevelLoad.cs
@@ -0,0 +1,17 @@
+﻿using GDEdit.Application;
+using static GDEdit.Utilities.Functions.GeometryDash.Gamesave;
+
+namespace GDE.App.Main.lvl
+{
+    public static class LevelLoad
+    {
+        public static void LoadLevel(int index)
+        {
+            TryDecryptLevelData(out Database.DecryptedLevelData);
+            GetKeyIndices();
+            GetLevels();
+            Database.UserLevels[index].LevelString = GetLevelString(index);
+            TryDecryptLevelString(index, out Database.UserLevels[index].DecryptedLevelString);
+        }
+    }
+}
