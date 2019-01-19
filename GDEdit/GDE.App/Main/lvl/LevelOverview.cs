0aec10f1dabaae7be027be3b2bac4207588d023d
AlFas <AlFasGD@users.noreply.github.com>
2019-01-19 21:16:17 +0200

    Fix broken shit

--- GDEdit/GDE.App/Main/lvl/LevelOverview.cs
+++ GDEdit/GDE.App/Main/lvl/LevelOverview.cs
@@ -1,22 +1,8 @@
-﻿using osu.Framework.Extensions.Color4Extensions;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
-using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.UserInterface;
-using osu.Framework.Graphics;
+﻿using GDEdit.Application;
 using osu.Framework.Screens;
-using osuTK;
-using osuTK.Graphics;
-using GDE.App.Main.Colours;
-using GDE.App.Main.UI;
-using GDE.App.Main.UI.Toolbar;
-using GDEdit.Application;
-using GDEdit.Application.Editor;
-using GDEdit.Utilities.Functions.GeometryDash;
-using GDEdit.Utilities.Objects.GeometryDash;
-using GDEdit.Utilities.Objects.GeometryDash.LevelObjects;
-using System.Collections.Generic;
+using System;
 using static GDEdit.Utilities.Functions.GeometryDash.Gamesave;
+using static GDEdit.Application.Database;
 
 namespace GDE.App.Main.lvl
 {
@@ -24,13 +10,14 @@ namespace GDE.App.Main.lvl
     {
         public LevelOverview()
         {
-            TryDecryptLevelData(out Database.DecryptedLevelData);
+            TryDecryptLevelData(out DecryptedLevelData);
             GetKeyIndices();
             GetLevels();
-            Database.UserLevels[0].LevelString = GetLevelString(0);
-            TryDecryptLevelString(0, out Database.UserLevels[0].DecryptedLevelString);
+            UserLevels[0].LevelString = GetLevelString(0);
+            TryDecryptLevelString(0, out UserLevels[0].DecryptedLevelString);
+            UserLevels[0].LevelObjects = GetObjects(GetObjectString(UserLevels[0].DecryptedLevelString));
 
-            System.Console.WriteLine(Database.UserLevels[0].LevelObjects[0]);
+            Console.WriteLine(UserLevels[0].LevelObjects[0]);
         }
     }
 }
