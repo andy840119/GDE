1d639ebbd5ad5771571b7b463b7ed623826f81a1
Styphix <altenyt@gmail.com>
2019-01-20 12:41:27 -0500

    Grid, Drag, More objects, etc

--- GDEdit/GDE.App/Main/Objects/ObjectBase.cs
+++ GDEdit/GDE.App/Main/Objects/ObjectBase.cs
@@ -94,9 +94,9 @@ namespace GDE.App.Main.Objects
             {
                 obj = new Box
                 {
-                    RelativeSizeAxes = Axes.Both,
                     Origin = Anchor.Centre,
                     Anchor = Anchor.Centre,
+                    RelativeSizeAxes = Axes.Both
                 }
             };
         }
