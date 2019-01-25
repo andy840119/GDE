b2c02bd9a7298397ea9deb61c705833bbfa58e74
AlFas <AlFasGD@users.noreply.github.com>
2019-01-25 22:00:59 +0200

    Add radius, width, height properties

---
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/ObjectHitboxes/IHasHeight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Objects.GeometryDash.ObjectHitboxes
+{
+    /// <summary>Represents a hitbox that has a height property.</summary>
+    public interface IHasHeight
+    {
+        /// <summary>The height of the hitbox.</summary>
+        double Height { get; set; }
+    }
+}
---
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/ObjectHitboxes/IHasWidth.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Objects.GeometryDash.ObjectHitboxes
+{
+    /// <summary>Represents a hitbox that has a width property.</summary>
+    public interface IHasWidth
+    {
+        /// <summary>The width of the hitbox.</summary>
+        double Width { get; set; }
+    }
+}
