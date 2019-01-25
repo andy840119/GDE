16b11853d5e8841fe473476b8ba8f96bced79b29
AlFas <AlFasGD@users.noreply.github.com>
2019-01-25 22:32:25 +0200

    Support hitbox behavior

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/ObjectHitboxes/Hitbox.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/ObjectHitboxes/Hitbox.cs
@@ -11,12 +11,26 @@ namespace GDEdit.Utilities.Objects.GeometryDash.ObjectHitboxes
     {
         /// <summary>The hitbox type of the hitbox.</summary>
         public HitboxType HitboxType { get; }
+        /// <summary>The behavior of the hitbox.</summary>
+        public HitboxBehavior Behavior { get; }
 
         /// <summary>Initializes a new instance of the <seealso cref="Hitbox"/> class.</summary>
         /// <param name="type">The hitbox type of the hitbox.</param>
-        public Hitbox(HitboxType type)
+        public Hitbox(HitboxType type, HitboxBehavior behavior = HitboxBehavior.Platform)
         {
             HitboxType = type;
+            Behavior = behavior;
         }
+    }
+
+    /// <summary>Represents a hitbox behavior.</summary>
+    public enum HitboxBehavior
+    {
+        /// <summary>Represents a hitbox that behaves as a platform, allowing the player to land on it.</summary>
+        Platform,
+        /// <summary>Represents a hitbox that behaves as a hazard, killing the player right as soon as they come in contact.</summary>
+        Hazard,
+        /// <summary>Represents a hitbox that triggers an effect as soon as the player contacts it.</summary>
+        Trigger,
     }
 }
