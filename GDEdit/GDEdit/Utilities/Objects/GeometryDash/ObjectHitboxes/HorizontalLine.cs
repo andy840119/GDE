72c4577d1f1c915ddd963e2de3c75bc3d070945c
AlFas <AlFasGD@users.noreply.github.com>
2019-01-25 22:35:57 +0200

    Add vertical and horizontal line hitboxes

---
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/ObjectHitboxes/HorizontalLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Objects.GeometryDash.ObjectHitboxes
+{
+    /// <summary>Represents a horizontal line hitbox.</summary>
+    public class HorizontalLine : HitboxType
+    {
+        /// <summary>Initializes a new instance of the <seealso cref="HorizontalLine"/> class.</summary>
+        public HorizontalLine() : base() { }
+    }
+}
---
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/ObjectHitboxes/VerticalLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Objects.GeometryDash.ObjectHitboxes
+{
+    /// <summary>Represents a vertical line hitbox.</summary>
+    public class VerticalLine : HitboxType
+    {
+        /// <summary>Initializes a new instance of the <seealso cref="VerticalLine"/> class.</summary>
+        public VerticalLine() : base() { }
+    }
+}
