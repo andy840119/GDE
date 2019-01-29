8dc2b0a9be3f5ad92ccc213a97a4fe3d53a5356c
AlFas <AlFasGD@users.noreply.github.com>
2019-01-30 00:10:09 +0200

    Add cloning methods for orbs

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/Orbs/RedOrb.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/Orbs/RedOrb.cs
@@ -19,5 +19,8 @@ namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.SpecialObjects.Orbs
 
         /// <summary>Initializes a new instance of the <seealso cref="RedOrb"/> class.</summary>
         public RedOrb() : base() { }
+
+        /// <summary>Returns a clone of this <seealso cref="RedOrb"/>.</summary>
+        public override GeneralObject Clone() => AddClonedInstanceInformation(new RedOrb());
     }
 }
