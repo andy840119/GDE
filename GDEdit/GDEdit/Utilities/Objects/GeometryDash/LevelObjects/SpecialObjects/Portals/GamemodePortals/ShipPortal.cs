69e810916d8f128e6ca4adb804f45e38a6b80e09
AlFas <AlFasGD@users.noreply.github.com>
2019-01-30 00:26:15 +0200

    Add cloning methods for gamemode portals

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/Portals/GamemodePortals/ShipPortal.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/SpecialObjects/Portals/GamemodePortals/ShipPortal.cs
@@ -28,5 +28,17 @@ namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.SpecialObjects.Port
 
         /// <summary>Initializes a new instance of the <seealso cref="ShipPortal"/> class.</summary>
         public ShipPortal() : base() { }
+
+        /// <summary>Returns a clone of this <seealso cref="ShipPortal"/>.</summary>
+        public override GeneralObject Clone() => AddClonedInstanceInformation(new ShipPortal());
+
+        /// <summary>Adds the cloned instance information and returns the cloned instance.</summary>
+        /// <param name="cloned">The cloned instance to add the information to.</param>
+        protected override GeneralObject AddClonedInstanceInformation(GeneralObject cloned)
+        {
+            var c = cloned as ShipPortal;
+            c.Checked = Checked;
+            return base.AddClonedInstanceInformation(c);
+        }
     }
 }
