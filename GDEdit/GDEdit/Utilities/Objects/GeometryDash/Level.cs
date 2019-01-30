c85f594d4b667f3f500fd9e0d893fffd052fc004
AlFas <AlFasGD@users.noreply.github.com>
2019-01-30 19:37:27 +0200

    Add support for the level objects in the level string

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/Level.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/Level.cs
@@ -168,7 +168,7 @@ namespace GDEdit.Utilities.Objects.GeometryDash
         /// <summary>Clones this level and returns the cloned result.</summary>
         public Level Clone() => new Level(new string(RawLevel.ToCharArray()));
         /// <summary>Returns the level string of this <seealso cref="Level"/>.</summary>
-        public string GetLevelString() => $"kS38,{ColorChannels},kA13,{SongOffset},kA15,{(FadeIn ? "1" : "0")},kA16,{(FadeOut ? "1" : "0")},kA14,{Guidelines},kA6,{BackgroundTexture},kA7,{GroundTexture},kA17,{GroundLine},kA18,{Font},kS39,0,kA2,{StartingGamemode},kA3,{StartingSize},kA8,{(DualMode ? "1" : "0")},kA4,{StartingSpeed},kA9,0,kA10,{(TwoPlayerMode ? "1" : "0")},kA11,{(InversedGravity ? "1" : "0")};";
+        public string GetLevelString() => $"kS38,{ColorChannels},kA13,{SongOffset},kA15,{(FadeIn ? "1" : "0")},kA16,{(FadeOut ? "1" : "0")},kA14,{Guidelines},kA6,{BackgroundTexture},kA7,{GroundTexture},kA17,{GroundLine},kA18,{Font},kS39,0,kA2,{StartingGamemode},kA3,{StartingSize},kA8,{(DualMode ? "1" : "0")},kA4,{StartingSpeed},kA9,0,kA10,{(TwoPlayerMode ? "1" : "0")},kA11,{(InversedGravity ? "1" : "0")};{LevelObjects}";
         /// <summary>Returns the raw level string of this <seealso cref="Level"/>.</summary>
         public string GetRawLevel() => $"<k>kCEK</k><i>4</i><k>k1</k><i>{ID}</i><k>k2</k><s>{Name}</s><k>k4</k><s>{LevelString}</s>{(Description.Length > 0 ? $"<k>k3</k><s>{ToBase64String(Encoding.ASCII.GetBytes(Description))}</s>" : "")}<k>k46</k><i>{Revision}</i><k>k5</k><s>{CreatorName}</s><k>k13</k><t />{GetBoolPropertyString("k14", VerifiedStatus)}{GetBoolPropertyString("k15", UploadedStatus)}{GetBoolPropertyString("k79", Unlisted)}<k>k21</k><i>2</i><k>k16</k><i>{Version}</i><k>k8</k><i>{OfficialSongID}</i><k>k45</k><i>{CustomSongID}</i><k>k80</k><i>{BuildTime}</i><k>k50</k><i>{BinaryVersion}</i><k>k47</k><t /><k>k84</k><i>{Folder}</i><k>kI1</k><r>{CameraX}</r><k>kI2</k><r>{CameraY}</r><k>kI3</k><r>{CameraZoom}</r>";
         #endregion
@@ -199,10 +199,11 @@ namespace GDEdit.Utilities.Objects.GeometryDash
         #region Private stuff
         private void GetLevelStringInformation(string levelString)
         {
-            string infoString = levelString.Split(';').First();
+            string infoString = levelString.Substring(0, levelString.IndexOf(';'));
             string[] split = infoString.Split(',');
             for (int i = 0; i < split.Length; i += 2)
                 GetLevelStringParameterInformation(split[i], split[i + 1]);
+            LevelObjects = GetObjects(GetObjectString(levelString));
         }
         private void GetRawInformation(string raw)
         {
