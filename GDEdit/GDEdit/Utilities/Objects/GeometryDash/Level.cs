b72dc0e581cec152db78a221cc7af86407bc1f4f
AlFas <AlFasGD@users.noreply.github.com>
2019-01-30 23:04:50 +0200

    Add attributes to appropriate properties

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/Level.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/Level.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GDEdit.Utilities.Attributes;
 using GDEdit.Utilities.Enumerations.GeometryDash;
 using GDEdit.Utilities.Functions.Extensions;
 using GDEdit.Utilities.Functions.General;
@@ -34,16 +35,21 @@ namespace GDEdit.Utilities.Objects.GeometryDash
         /// <summary>The name of the creator.</summary>
         public string CreatorName { get; set; }
         /// <summary>The revision of the level.</summary>
+        [CommonMergedProperty]
         public int Revision { get; set; }
         /// <summary>The attempts made in the level.</summary>
         public int Attempts { get; set; }
         /// <summary>The ID of the level.</summary>
+        [CommonMergedProperty]
         public int ID { get; set; }
         /// <summary>The version of the level.</summary>
+        [CommonMergedProperty]
         public int Version { get; set; }
         /// <summary>The folder of the level.</summary>
+        [CommonMergedProperty]
         public int Folder { get; set; }
         /// <summary>The password of the level.</summary>
+        [CommonMergedProperty]
         public int Password { get; set; }
         /// <summary>The binary version of the game the level was created on.</summary>
         public int BinaryVersion { get; set; }
@@ -56,45 +62,63 @@ namespace GDEdit.Utilities.Objects.GeometryDash
             set => BuildTime = (int)value.TotalSeconds;
         }
         /// <summary>Determines whether the level has been verified or not.</summary>
+        [CommonMergedProperty]
         public bool VerifiedStatus { get; set; }
         /// <summary>Determines whether the level has been uploaded or not.</summary>
+        [CommonMergedProperty]
         public bool UploadedStatus { get; set; }
         /// <summary>Determines whether the level is unlisted or not.</summary>
+        [CommonMergedProperty]
         public bool Unlisted { get; set; }
         /// <summary>The length of the level.</summary>
         public LevelLength Length { get; set; }
 
         // Level properties
         /// <summary>The official song ID used in the level.</summary>
+        [CommonMergedProperty]
         public int OfficialSongID { get; set; }
         /// <summary>The custom song ID used in the level.</summary>
+        [CommonMergedProperty]
         public int CustomSongID { get; set; }
         /// <summary>The song offset of the level.</summary>
+        [CommonMergedProperty]
         public int SongOffset { get; set; }
         /// <summary>The fade in property of the song of the level.</summary>
+        [CommonMergedProperty]
         public bool FadeIn { get; set; }
         /// <summary>The fade out property of the song of the level.</summary>
+        [CommonMergedProperty]
         public bool FadeOut { get; set; }
 
         /// <summary>The starting speed of the player when the level begins.</summary>
+        [CommonMergedProperty]
         public Speed StartingSpeed { get; set; }
         /// <summary>The starting gamemode of the player when the level begins.</summary>
+        [CommonMergedProperty]
         public Gamemode StartingGamemode { get; set; }
         /// <summary>The starting size of the player when the level begins.</summary>
+        [CommonMergedProperty]
         public PlayerSize StartingSize { get; set; }
         /// <summary>The Dual Mode property of the level.</summary>
+        [CommonMergedProperty]
         public bool DualMode { get; set; }
         /// <summary>The 2-Player Mode property of the level.</summary>
+        [CommonMergedProperty]
         public bool TwoPlayerMode { get; set; }
         /// <summary>The inversed gravity property of the level (there is no ability to set that from the game itself without hacking the gamesave).</summary>
+        [CommonMergedProperty]
         public bool InversedGravity { get; set; }
         /// <summary>The background texture property of the level.</summary>
+        [CommonMergedProperty]
         public int BackgroundTexture { get; set; }
         /// <summary>The ground texture property of the level.</summary>
+        [CommonMergedProperty]
         public int GroundTexture { get; set; }
         /// <summary>The ground line property of the level.</summary>
+        [CommonMergedProperty]
         public int GroundLine { get; set; }
         /// <summary>The font property of the level.</summary>
+        [CommonMergedProperty]
         public int Font { get; set; }
         /// <summary>The color channels of the level.</summary>
         public LevelColorChannels ColorChannels { get; private set; }
@@ -138,10 +162,13 @@ namespace GDEdit.Utilities.Objects.GeometryDash
 
         // Editor stuff
         /// <summary>The X position of the camera.</summary>
+        [CommonMergedProperty]
         public double CameraX { get; set; }
         /// <summary>The Y position of the camera.</summary>
+        [CommonMergedProperty]
         public double CameraY { get; set; }
         /// <summary>The zoom of the camera.</summary>
+        [CommonMergedProperty]
         public double CameraZoom { get; set; }
 
         // Strings
