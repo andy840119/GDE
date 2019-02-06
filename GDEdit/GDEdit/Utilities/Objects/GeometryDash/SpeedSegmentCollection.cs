32f7e67cd171565f5620c52097e9e3aef485e26f
AlFas <AlFasGD@users.noreply.github.com>
2019-02-06 18:04:36 +0200

    Add function to convert X to time

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/SpeedSegmentCollection.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/SpeedSegmentCollection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static GDEdit.Utilities.Information.GeometryDash.Speeds;
 
 namespace GDEdit.Utilities.Objects.GeometryDash
 {
@@ -14,6 +15,18 @@ namespace GDEdit.Utilities.Objects.GeometryDash
         /// <summary>Initializes a new instance of the <seealso cref="SpeedSegmentCollection"/> class.</summary>
         /// <param name="segments">The speed segments to create this <seealso cref="SpeedSegmentCollection"/> from.</param>
         public SpeedSegmentCollection(SortedSet<SpeedSegment> segments) : base(segments) { }
+
+        /// <summary>Converts the provided X position into time.</summary>
+        /// <param name="x">The X position to convert into time.</param>
+        public double ConvertXToTime(double x)
+        {
+            double time = 0;
+            int i = 1;
+            for (; i < Count && this[i].X < x; i++)
+                time += (this[i].X - this[i - 1].X) * GetSpeed(this[i - 1].Speed);
+            int final = Math.Max(i, Count);
+            return time + (x - this[final].X) * GetSpeed(this[final].Speed);
+        }
 
         /// <summary>Gets or sets the element at the specified index.</summary>
         /// <param name="index">The index of the element to get or set.</param>
