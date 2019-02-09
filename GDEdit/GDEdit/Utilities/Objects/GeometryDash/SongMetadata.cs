d6fd056fc94fd1eea988cb7f7148679bf524a636
AlFas <AlFasGD@users.noreply.github.com>
2019-02-09 19:02:30 +0200

    Add song metadata class

---
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/SongMetadata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Objects.GeometryDash
+{
+    /// <summary>Contains the metadata of a song.</summary>
+    public class SongMetadata
+    {
+        /// <summary>The ID of the song.</summary>
+        public int ID { get; set; }
+        /// <summary>The title of the song.</summary>
+        public string Title { get; set; }
+        /// <summary>The author of the song.</summary>
+        public string Author { get; set; }
+        /// <summary>The URL to the song on Newgrounds.</summary>
+        public string URL => $"https://www.newgrounds.com/audio/listen/{ID}";
+
+        /// <summary>Initializes a new instance of the <seealso cref="SongMetadata"/> class.</summary>
+        public SongMetadata() { }
+    }
+}
