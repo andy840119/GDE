317024a41900ae4b14b3722368ca0e7869e96564
AlFas <AlFasGD@users.noreply.github.com>
2019-02-23 21:41:48 +0200

    Add necessary structs

--- GDEdit/GDEdit/Utilities/Attributes/FutureProofingAttribute.cs
+++ GDEdit/GDEdit/Utilities/Attributes/FutureProofingAttribute.cs
@@ -8,7 +8,7 @@ using GDEdit.Utilities.Enumerations.GeometryDash;
 namespace GDEdit.Utilities.Attributes
 {
     /// <summary>Represents that a feature is only supported for future-proofing in the game, as suggested by teasers and trailers and may be subject to changes.</summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field, Inherited = false, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Property | AttributeTargets.Field, Inherited = false, AllowMultiple = true)]
     public sealed class FutureProofingAttribute : Attribute
     {
         /// <summary>The version of the expected version that the feature will be implemented in the game.</summary>
---
+++ GDEdit/GDEdit/Utilities/Objects/General/Color.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDEdit.Utilities.Objects.General
+{
+    /// <summary>Represents an ARGB color.</summary>
+    public struct Color
+    {
+        private ColorValue r, g, b, a;
+
+        /// <summary>The red value of the color.</summary>
+        public float R
+        {
+            get => r;
+            set => r = value;
+        }
+        /// <summary>The green value of the color.</summary>
+        public float G
+        {
+            get => g;
+            set => g = value;
+        }
+        /// <summary>The blue value of the color.</summary>
+        public float B
+        {
+            get => b;
+            set => b = value;
+        }
+        /// <summary>The alpha value of the color.</summary>
+        public float A
+        {
+            get => a;
+            set => a = value;
+        }
+        
+        /// <summary>Initializes a new instance of the <seealso cref="Color"/> class.</summary>
+        /// <param name="all">The value for all the color values (R, G, B, A).</param>
+        public Color(float all) : this(all, all, all, all) { }
+        /// <summary>Initializes a new instance of the <seealso cref="Color"/> class.</summary>
+        /// <param name="red">The red value of the color.</param>
+        /// <param name="green">The green value of the color.</param>
+        /// <param name="blue">The blue value of the color.</param>
+        /// <param name="alpha">The alpha value of the color.</param>
+        public Color(float red, float green, float blue, float alpha)
+        {
+            r = red;
+            g = green;
+            b = blue;
+            a = alpha;
+        }
+
+        private struct ColorValue
+        {
+            private float v;
+
+            public float Value
+            {
+                get => v;
+                set
+                {
+                    if (value < 0 || value > 1)
+                        throw new ArgumentException("The color value cannot be outside the range [0, 1].");
+                    v = value;
+                }
+            }
+
+            public ColorValue(float value)
+            {
+                v = value;
+                Value = v;
+            }
+
+            public static implicit operator float(ColorValue v) => v.v;
+            public static implicit operator ColorValue(float f) => new ColorValue(f);
+        }
+    }
+}
