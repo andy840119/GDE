1f99c84c42c91df7d3be28dc6837ccd10c268b9d
AlFas <AlFasGD@users.noreply.github.com>
2019-03-03 21:52:52 +0200

    Improve casting performance

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/GeneralObject.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/GeneralObject.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using GDEdit.Utilities.Attributes;
 using GDEdit.Utilities.Enumerations.GeometryDash;
+using GDEdit.Utilities.Functions.Extensions;
 using GDEdit.Utilities.Functions.GeometryDash;
 using GDEdit.Utilities.Information.GeometryDash;
 using GDEdit.Utilities.Objects.General;
@@ -122,8 +123,8 @@ namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects
         [ObjectStringMappable(ObjectParameter.GroupIDs)]
         public int[] GroupIDs
         {
-            get => groupIDs.Cast<int>() as int[];
-            set => groupIDs = value.Cast<short>() as short[];
+            get => groupIDs.CastToInt();
+            set => groupIDs = value.CastToShort();
         }
         /// <summary>The linked group ID of this object.</summary>
         [ObjectStringMappable(ObjectParameter.LinkedGroupID)]
