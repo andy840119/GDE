312531abf813cd70f9f84a7661d553fb2582355f
AlFas <AlFasGD@users.noreply.github.com>
2019-06-15 04:35:21 +0300

    Migrate class to another file

--- GDEdit/GDEdit/Application/Editor/Editor.cs
+++ GDEdit/GDEdit/Application/Editor/Editor.cs
@@ -1390,54 +1390,6 @@ namespace GDEdit.Application.Editor
             if (o is IHasTargetGroupID t && r.IsWithinSourceRange(t.TargetGroupID))
                 t.TargetGroupID += d;
         }
-
-        public class SourceTargetRange
-        {
-            public int SourceFrom;
-            public int SourceTo;
-            public int TargetFrom;
-            public int TargetTo;
-
-            public int Range => SourceTo - SourceFrom;
-            public int Difference => TargetFrom - SourceFrom;
-
-            public SourceTargetRange(int sourceFrom, int sourceTo, int targetFrom, int targetTo)
-            {
-                SourceFrom = sourceFrom;
-                SourceTo = sourceTo;
-                TargetFrom = targetFrom;
-                TargetTo = targetTo;
-            }
-
-            public bool IsWithinSourceRange(int value) => SourceFrom <= value && value <= SourceTo;
-
-            public static SourceTargetRange Parse(string str)
-            {
-                string[,] split = str.Split('>').Split('-');
-                int length0 = split.GetLength(0);
-                int length1 = split.GetLength(1);
-
-                for (int i = 0; i < length0; i++)
-                    for (int j = 0; j < length1; j++)
-                    {
-                        while (split[i, j].First() == ' ')
-                            split[i, j] = split[i, j].Remove(0, 1);
-                        while (split[i, j].Last() == ' ')
-                            split[i, j] = split[i, j].Remove(split[i, j].Length - 1, 1);
-                    }
-                return new SourceTargetRange(ToInt32(split[0, 0]), ToInt32(split[0, length1 - 1]), ToInt32(split[1, 0]), ToInt32(split[1, length1 - 1]));
-            }
-            public static List<SourceTargetRange> LoadRangesFromStringArray(string[] lines, bool ignoreEmptyLines = true)
-            {
-                var list = new List<SourceTargetRange>();
-                foreach (string s in lines)
-                    if (!ignoreEmptyLines || s != "")
-                        list.Add(Parse(s));
-                return list;
-            }
-
-            public override string ToString() => $"{SourceFrom}{(Range > 0 ? $"-{SourceTo}" : "")} > {TargetFrom}{(Range > 0 ? $"-{TargetTo}" : "")}";
-        }
         #endregion
 
         #region Other
