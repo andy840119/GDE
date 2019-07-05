c0c8d9b3f89df9aad073beecd769cddcfd957e45
AlFas <AlFasGD@users.noreply.github.com>
2019-07-05 19:39:22 +0300

    Add number text box

---
+++ GDEdit/GDE.App/Main/UI/NumberTextBox.cs
@@ -0,0 +1,30 @@
+﻿using osu.Framework.Graphics.UserInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Convert;
+using static System.Char;
+using osu.Framework.Bindables;
+
+namespace GDE.App.Main.UI
+{
+    public class NumberTextBox : TextBox
+    {
+        /// <summary>Gets the current numeric value in the textbox.</summary>
+        public int Number => Current.Value.Length > 0 ? ToInt32(Current.Value) : 0;
+
+        public event Action<int> NumberChanged;
+
+        public NumberTextBox() : this(0) { }
+        public NumberTextBox(int startingValue)
+            : base()
+        {
+            Text = startingValue.ToString();
+            Current.ValueChanged += OnNumberChanged;
+        }
+
+        protected override bool CanAddCharacter(char character) => IsNumber(character);
+
+        private void OnNumberChanged(ValueChangedEvent<string> v) => NumberChanged?.Invoke(ToInt32(v.NewValue));
+    }
+}
