fe4a2861dbe91dc23cf829b67cc323954651d298
AlFas <AlFasGD@users.noreply.github.com>
2019-07-08 12:50:45 +0300

    Improve ID migration card and add test case

--- GDEdit/GDE.App/Main/Screens/Edit/Components/IDMigrationStepCard.cs
+++ GDEdit/GDE.App/Main/Screens/Edit/Components/IDMigrationStepCard.cs
@@ -9,7 +9,9 @@ using GDEdit.Utilities.Objects.GeometryDash.LevelObjects;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Configuration;
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
@@ -23,15 +25,23 @@ namespace GDE.App.Main.Screens.Edit.Components
     public class IDMigrationStepCard : ClickableContainer, IFilterable
     {
         private bool matchingFilter, filteringActive;
+        private int index;
 
         private Box selectionBar;
-        private Box stepIndexContainer;
         private Box hoverBox;
+        private Box stepIndexContainerBackground;
+        private Container stepIndexContainer;
         private DraggableCardContainer stepDragContainer;
+        private SpriteText stepIndex;
         private SpriteText rightArrow, sourceText, targetText;
 
-        public Bindable<bool> Selected = new Bindable<bool>(false);
-        public int Index;
+        public readonly Bindable<bool> Selected = new Bindable<bool>(false);
+
+        public int Index
+        {
+            get => index;
+            set => stepIndex.Text = (index = value).ToString();
+        }
 
         public readonly SourceTargetRange StepRange;
 
@@ -57,7 +67,6 @@ namespace GDE.App.Main.Screens.Edit.Components
             StepRange.TargetTo.ToString()
         };
         
-
         public IDMigrationStepCard(SourceTargetRange range)
         {
             StepRange = range;
@@ -65,70 +74,112 @@ namespace GDE.App.Main.Screens.Edit.Components
             CornerRadius = 5;
             Masking = true;
 
+            RelativeSizeAxes = Axes.X;
+            Height = 25;
+
             Children = new Drawable[]
             {
                 hoverBox = new Box
                 {
                     RelativeSizeAxes = Axes.Both,
-                    Colour = GDEColors.FromHex("161616")
-                },
-                selectionBar = new Box
-                {
-                    RelativeSizeAxes = Axes.Y,
-                    Size = new Vector2(5, 1),
-                    Colour = GDEColors.FromHex("202020")
-                },
-                selectionBar = new Box
-                {
-                    RelativeSizeAxes = Axes.Y,
-                    Size = new Vector2(20, 1),
-                    Colour = GDEColors.FromHex("404040")
-                },
-                rightArrow = new SpriteText
-                {
-                    Text = ">",
-                    Font = new FontUsage(size: 14),
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    Alpha = 0.5f,
+                    Colour = GDEColors.FromHex("181818")
                 },
                 new FillFlowContainer
                 {
-                    Direction = FillDirection.Vertical,
+                    Direction = FillDirection.Horizontal,
+                    RelativeSizeAxes = Axes.Y,
+
+                    Children = new Drawable[]
+                    {
+                        selectionBar = new Box
+                        {
+                            RelativeSizeAxes = Axes.Y,
+                            Width = 5,
+                            Colour = GDEColors.FromHex("808080"),
+                            Alpha = 0.5f, // Transparency will allow the background to lighten the container up
+                        },
+                        stepIndexContainer = new Container
+                        {
+                            RelativeSizeAxes = Axes.Y,
+                            Width = 25,
+
+                            Children = new Drawable[]
+                            {
+                                stepIndexContainerBackground = new Box
+                                {
+                                    RelativeSizeAxes = Axes.Both,
+                                    Colour = new ColourInfo
+                                    {
+                                        BottomLeft = GDEColors.FromHex("606060"),
+                                        BottomRight = GDEColors.FromHex("606060"),
+                                        TopLeft = GDEColors.FromHex("606060"),
+                                        TopRight = GDEColors.FromHex("606060"),
+                                    },
+                                    Alpha = 0.5f,
+                                },
+                                stepIndex = new SpriteText
+                                {
+                                    Anchor = Anchor.Centre,
+                                    Origin = Anchor.Centre,
+                                    Text = Index.ToString(),
+                                    Font = new FontUsage(size: 20),
+                                },
+                            },
+                        },
+
+                    }
+                },
+                // TODO: Migrate to a class called IDMigrationStepText
+                new Container
+                {
                     RelativeSizeAxes = Axes.Both,
-                    // TODO: Migrate to a class called IDMigrationStepText
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+
                     Children = new Drawable[]
                     {
                         sourceText = new SpriteText
                         {
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                            X = -100,
                             Text = range.SourceToString(),
-                            Font = new FontUsage(size: 14)
+                            Font = new FontUsage(size: 20),
+                        },
+                        rightArrow = new SpriteText
+                        {
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                            Text = ">",
+                            Font = new FontUsage(size: 20),
+                            Alpha = 0.5f,
                         },
                         targetText = new SpriteText
                         {
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                            X = 100,
                             Text = range.TargetToString(),
-                            Font = new FontUsage(size: 14)
+                            Font = new FontUsage(size: 20),
                         },
                     }
                 },
                 stepDragContainer = new DraggableCardContainer
                 {
                     RelativeSizeAxes = Axes.Y,
+                    Anchor = Anchor.CentreRight,
+                    Origin = Anchor.CentreRight,
                     Children = new Drawable[]
                     {
-                        new Box
-                        {
-                            RelativeSizeAxes = Axes.Y,
-                            Size = new Vector2(25, 1),
-                            Colour = GDEColors.FromHex("404040"),
-                            
-                        },
                         new SpriteIcon
                         {
                             Icon = FontAwesome.Solid.Bars,
                             Origin = Anchor.Centre,
                             Anchor = Anchor.Centre,
-                            RelativeSizeAxes = Axes.Both
+                            RelativeSizeAxes = Axes.Both,
+                            Size = new Vector2(0.7f),
+                            Y = -1, // I have no fucking idea why this happens, but it's necessary to make the sprite appear centered
+                            Alpha = 0.6f,
                         }
                     },
                     CardDragged = e =>
@@ -137,36 +188,50 @@ namespace GDE.App.Main.Screens.Edit.Components
                         CardDragged?.Invoke(e);
                     },
                     Size = new Vector2(25, 1),
-                    Colour = GDEColors.FromHex("404040")
                 },
             };
 
             Selected.ValueChanged += OnSelected;
-
+            StepRange.SourceTargetRangeChanged += (a, b, c, d) =>
+            {
+                sourceText.Text = StepRange.SourceToString();
+                targetText.Text = StepRange.TargetToString();
+            };
         }
 
         /// <summary>Initializes the right arrow's animation. This function should be only called once.</summary>
         public void InitializeArrowAnimation()
         {
             rightArrow
-                .MoveToOffset(new Vector2(20, 0), 125, Easing.InElastic).FadeTo(0)
-                .Then(125)
+                .MoveToOffset(new Vector2(20, 0), 500, Easing.InQuint).FadeTo(0, 500, Easing.InQuint)
+                .Then()
                 .MoveToOffset(new Vector2(-40, 0)) // Reset label position
                 .Then()
-                .MoveToOffset(new Vector2(20, 0), 125, Easing.InElastic).FadeTo(0.5f)
-                .Loop(3000);
+                .MoveToOffset(new Vector2(20, 0), 500, Easing.OutQuint).FadeTo(0.5f, 500, Easing.OutQuint)
+                .Loop(2000);
         }
 
-        private void OnSelected(ValueChangedEvent<bool> value) => selectionBar.FadeColour(GDEColors.FromHex(value.OldValue ? "202020" : "00bc5c"), 200);
+        private void OnSelected(ValueChangedEvent<bool> value)
+        {
+            var newColor = GDEColors.FromHex(value.OldValue ? "808080" : "00ffb8");
+            selectionBar.FadeColour(newColor, 200);
+            stepIndexContainerBackground.FadeColour(new ColourInfo
+            {
+                BottomLeft = newColor.Darken(0.25f),
+                TopLeft = newColor.Darken(0.25f),
+                BottomRight = GDEColors.FromHex("606060"),
+                TopRight = GDEColors.FromHex("606060"),
+            }, 200);
+        }
 
         protected override bool OnHover(HoverEvent e)
         {
-            hoverBox.FadeColour(GDEColors.FromHex("323232"), 500);
+            hoverBox.FadeColour(GDEColors.FromHex("303030"), 500);
             return base.OnHover(e);
         }
         protected override void OnHoverLost(HoverLostEvent e)
         {
-            hoverBox.FadeColour(GDEColors.FromHex("161616"), 500);
+            hoverBox.FadeColour(GDEColors.FromHex("181818"), 500);
             base.OnHoverLost(e);
         }
 
---
+++ GDEdit/GDE.Tests/Visual/TestCaseEditor/TestCaseIDMigrationStepCard.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GDE.App.Main.Screens.Edit;
+using GDE.App.Main.Screens.Edit.Components;
+using GDEdit.Application.Editor;
+using GDEdit.Utilities.Objects.General;
+using GDEdit.Utilities.Objects.GeometryDash;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Screens;
+using osu.Framework.Testing;
+using osuTK;
+using static GDE.App.Main.Screens.Edit.Components.IDMigrationStepList;
+
+namespace GDE.Tests.Visual.TestCaseEditor
+{
+    public class TestCaseIDMigrationStepCard : TestCase
+    {
+        private IDMigrationStepCard card;
+
+        public override IReadOnlyList<Type> RequiredTypes => new Type[] { typeof(IDMigrationStepCard) };
+
+        public TestCaseIDMigrationStepCard()
+        {
+            card = new IDMigrationStepCard(new SourceTargetRange(1, 2, 3))
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Width = 0.5f,
+                Index = 0,
+            };
+            Add(card);
+
+            AddStep("Set index to 0", () => card.Index = 0);
+            AddAssert("Check index is 0", () => card.Index == 0);
+            AddStep("Set index to 3", () => card.Index = 3);
+            AddAssert("Check index is 3", () => card.Index == 3);
+
+            AddStep("Set SourceTo to 14", () => card.StepRange.SourceTo = 14);
+            AddAssert("Check SourceTo is 14", () => card.StepRange.SourceTo == 14);
+            AddAssert("Check TargetTo is 16", () => card.StepRange.TargetTo == 16);
+            AddStep("Set SourceFrom to 10", () => card.StepRange.SourceFrom = 10);
+            AddAssert("Check SourceFrom is 10", () => card.StepRange.SourceFrom == 10);
+            AddAssert("Check TargetTo is 7", () => card.StepRange.TargetTo == 7);
+
+            AddStep("Select", () => card.Selected.Value = true);
+            AddWaitStep("Wait for animation to be completed", 1);
+            AddStep("Deselect", () => card.Selected.Value = false);
+            AddWaitStep("Wait for animation to be completed", 1);
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            card.InitializeArrowAnimation();
+        }
+    }
+}
