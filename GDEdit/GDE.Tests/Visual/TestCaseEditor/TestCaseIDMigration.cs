91930557d56f34419e869b9c1b6eedd359365fc4
AlFas <AlFasGD@users.noreply.github.com>
2019-07-10 14:08:21 +0300

    Add test steps for the ID migration screen

--- GDEdit/GDE.Tests/Visual/TestCaseEditor/TestCaseIDMigration.cs
+++ GDEdit/GDE.Tests/Visual/TestCaseEditor/TestCaseIDMigration.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using GDE.App.Main.Screens.Edit;
 using GDE.App.Main.Screens.Edit.Components;
 using GDEdit.Application.Editor;
+using GDEdit.Utilities.Objects.General;
 using GDEdit.Utilities.Objects.GeometryDash;
 using osu.Framework.Graphics;
 using osu.Framework.Screens;
@@ -16,10 +17,59 @@ namespace GDE.Tests.Visual.TestCaseEditor
 
         public TestCaseIDMigration()
         {
-            Add(new ScreenStack(new IDMigrationScreen(new Editor(new Level())))
+            var screen = new IDMigrationScreen(new Editor(new Level()));
+
+            Add(new ScreenStack(screen)
             {
-                RelativeSizeAxes = Axes.Both
+                RelativeSizeAxes = Axes.Both,
             });
+
+            var stepList = screen.StepList;
+
+            AddLabel("Step creation");
+            AddAssert("Check whether common step is null", () => screen.CommonIDMigrationStep.Value == null); // Also a dummy to not fuck the tests
+            AddStep("Create a new step", () => stepList.AddStep(new SourceTargetRange(1, 5, 15)));
+            AddStep("Select step at index 0", () => stepList.SelectStep(0));
+            AddAssert("Check common selected values", () => screen.CommonIDMigrationStep.Value.HasValues(1, 5, 15, 19));
+            AddStep("Create a new step", () => stepList.AddStep(new SourceTargetRange(1, 5, 15)));
+            AddStep("Select step at index 1", () => stepList.SelectStep(1));
+            AddAssert("Check common selected values", () => screen.CommonIDMigrationStep.Value.HasValues(1, 5, 15, 19));
+            AddStep("Create a new step", () => stepList.AddStep(new SourceTargetRange(20, 30, 40)));
+            AddStep("Select step at index 2", () => stepList.SelectStep(2));
+            AddAssert("Check common selected values", () => screen.CommonIDMigrationStep.Value.HasValues(20, 30, 40, 50));
+
+            AddLabel("Step selection");
+            AddStep("Deselect step at index 2", () => stepList.DeselectStep(2));
+            AddStep("Append step at index 0 to selection", () => stepList.SelectStep(0, true));
+            AddStep("Append step at index 1 to selection", () => stepList.SelectStep(1, true));
+            AddAssert("Match current selection", () => stepList.SelectedStepIndices.SetEquals(new int[] { 0, 1 }));
+            AddAssert("Check common selected values", () => screen.CommonIDMigrationStep.Value.HasValues(1, 5, 15, 19));
+            AddStep("Append step at index 2 to selection", () => stepList.SelectStep(2, true));
+            AddAssert("Match current selection", () => stepList.SelectedStepIndices.SetEquals(new int[] { 0, 1, 2 }));
+            AddAssert("Check common selected values", () => screen.CommonIDMigrationStep.Value.HasValues(-1, -1, -1, -1));
+            AddStep("Deselect all", () => stepList.DeselectAll());
+            AddAssert("Match current selection", () => stepList.SelectedStepIndices.SetEquals(new int[] { }));
+            AddAssert("Check common selected values", () => screen.CommonIDMigrationStep.Value == null);
+            AddStep("Select all", () => stepList.SelectAll());
+            AddAssert("Match current selection", () => stepList.SelectedStepIndices.SetEquals(new int[] { 0, 1, 2 }));
+            AddAssert("Check common selected values", () => screen.CommonIDMigrationStep.Value.HasValues(-1, -1, -1, -1));
+
+            AddLabel("Step removal");
+            AddStep("Remove currently selected steps", () => stepList.RemoveSelectedSteps());
+            AddAssert("Check whether common step is null", () => screen.CommonIDMigrationStep.Value == null);
+
+            AddLabel("Step cloning");
+            AddRepeatStep("Create 4 new identical steps", () => stepList.AddStep(new SourceTargetRange(1, 5, 15)), 4);
+            AddStep("Select newly created steps", () =>
+            {
+                for (int i = 0; i < 4; i++)
+                    stepList.SelectStep(i, true);
+            });
+            AddAssert("Check common selected values", () => screen.CommonIDMigrationStep.Value.HasValues(1, 5, 15, 19));
+            AddAssert("Match current selection", () => stepList.SelectedStepIndices.SetEquals(new int[] { 0, 1, 2, 3 }));
+            AddStep("Clone currently selected steps", () => stepList.CloneSelectedSteps());
+            AddAssert("Match current selection", () => stepList.SelectedStepIndices.SetEquals(new int[] { 4, 5, 6, 7 }));
+            AddAssert("Check common selected values", () => screen.CommonIDMigrationStep.Value.HasValues(1, 5, 15, 19));
         }
     }
 }
