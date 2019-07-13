ad54295fbbd1734593e017f0033c99a0ceb1abfc
AlFas <AlFasGD@users.noreply.github.com>
2019-07-13 15:00:35 +0300

    Create ID migration tab control and item classes

---
+++ GDEdit/GDE.App/Main/Screens/Edit/Components/IDMigration/IDMigrationTabControl.cs
@@ -0,0 +1,65 @@
+﻿using GDE.App.Main.Colors;
+using GDE.App.Main.UI;
+using GDEdit.Application.Editor;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Input.Events;
+using osuTK;
+using osuTK.Graphics;
+using System;
+using BaseMenu = osu.Framework.Graphics.UserInterface.Menu;
+
+namespace GDE.App.Main.Screens.Edit.Components.IDMigration
+{
+    public class IDMigrationTabControl : Container
+    {
+        private IDMigrationTabItem[] tabItems = new IDMigrationTabItem[4];
+        private IDMigrationTabItem currentTab;
+
+        private FillFlowContainer itemsContainer;
+
+        public event Action<IDMigrationMode> TabSelected;
+
+        public IDMigrationTabControl()
+            : base()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                tabItems[i] = new IDMigrationTabItem((IDMigrationMode)i);
+                tabItems[i].TabSelected += HandleTabSelected;
+            }
+            currentTab = this[IDMigrationMode.Groups];
+
+            RelativeSizeAxes = Axes.X;
+            Height = 32;
+
+            Children = new Drawable[]
+            {
+                new Box
+                {
+                    Colour = GDEColors.FromHex("111")
+                },
+                itemsContainer = new FillFlowContainer
+                {
+                    Direction = FillDirection.Horizontal,
+                    RelativeSizeAxes = Axes.Both,
+                    Padding = new MarginPadding { Left = 10 },
+                    Children = tabItems,
+                }
+            };
+        }
+
+        private IDMigrationTabItem this[IDMigrationMode mode] => tabItems[(int)mode];
+
+        private void HandleTabSelected(IDMigrationMode newMode)
+        {
+            currentTab.MoveToOffset(new Vector2(0, -10), 500, Easing.OutQuint);
+            (currentTab = this[newMode]).MoveToOffset(new Vector2(0, 10), 500, Easing.InQuint);
+            TabSelected?.Invoke(newMode);
+        }
+    }
+}
---
+++ GDEdit/GDE.App/Main/Screens/Edit/Components/IDMigration/IDMigrationTabItem.cs
@@ -0,0 +1,86 @@
+﻿using GDE.App.Main.Colors;
+using GDEdit.Application.Editor;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Input.Events;
+using osuTK.Graphics;
+using System;
+
+namespace GDE.App.Main.Screens.Edit.Components.IDMigration
+{
+    public class IDMigrationTabItem : Container
+    {
+        private static Color4 selectedBackgroundColor = GDEColors.FromHex("606060");
+        private static Color4 hoveredBackgroundColor = GDEColors.FromHex("484848");
+        private static Color4 backgroundColor = GDEColors.FromHex("303030");
+
+        private Box background;
+
+        private bool selected;
+
+        public bool Selected
+        {
+            get => selected;
+            set
+            {
+                bool old = selected;
+                background.FadeColour((selected = value) ? selectedBackgroundColor : (IsHovered ? hoveredBackgroundColor : backgroundColor), 200);
+                if (!old)
+                    TabSelected?.Invoke(Mode);
+            }
+        }
+
+        public readonly IDMigrationMode Mode;
+
+        public event Action<IDMigrationMode> TabSelected;
+
+        public IDMigrationTabItem(IDMigrationMode mode)
+            : base()
+        {
+            Mode = mode;
+
+            Anchor = Anchor.Centre;
+            Origin = Anchor.Centre;
+
+            CornerRadius = 10;
+            Masking = true;
+
+            Children = new Drawable[]
+            {
+                new SpriteText
+                {
+                    Font = new FontUsage(size: 16),
+                    Text = mode.ToString(),
+                },
+                background = new Box
+                {
+                    Colour = backgroundColor,
+                }
+            };
+        }
+
+        protected override bool OnHover(HoverEvent e)
+        {
+            if (!Selected)
+                background.FadeColour(hoveredBackgroundColor, 200);
+
+            return base.OnHover(e);
+        }
+        protected override void OnHoverLost(HoverLostEvent e)
+        {
+            if (!Selected)
+                background.FadeColour(backgroundColor, 200);
+
+            base.OnHoverLost(e);
+        }
+
+        protected override bool OnClick(ClickEvent e)
+        {
+            Selected = true;
+            return base.OnClick(e);
+        }
+    }
+}
