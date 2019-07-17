4ccd49c33ddf048b4d2a1061b13ec148f6b8c7aa
AlFas <AlFasGD@users.noreply.github.com>
2019-07-17 22:47:52 +0300

    I said now we have our own fucking button

--- GDEdit/GDE.App/Main/UI/GDEButton.cs
+++ GDEdit/GDE.App/Main/UI/GDEButton.cs
@@ -1,9 +1,8 @@
 ﻿using GDE.App.Main.Colors;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
 using osuTK.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace GDE.App.Main.UI
 {
@@ -12,9 +11,29 @@ namespace GDE.App.Main.UI
         public GDEButton()
             : base()
         {
-            CornerRadius = 5;
-            Colour = Color4.White;
+            Height = 30;
+            CornerRadius = 3;
+            Masking = true;
+
             BackgroundColour = GDEColors.FromHex("303030");
         }
+
+        // Okay peppy we need to fucking talk like men
+        // Setting the color of the button's text to yellow by default on the framework is real mad shit
+        // This is pretty much a crime against fundamental framework rules
+        // While you wanted the text to appear yellow for whatever fucking reason (that color fucking sucks btw), you cannot personalize it on a framework level
+        // It makes the most sense for the default color to be white
+        // Since it is a neutral color and clashes with the background color which defaults to black IIRC
+        // Therefore you've done mad shit and you cannot be forgiven anymore
+        // Having to overload this function just because you decided to change the default color to something ugly is so bullshit
+        // Just make a fucking FrameworkButton and overload the function there for your beautiful yellow color ffs
+        protected override SpriteText CreateText() => new SpriteText
+        {
+            Anchor = Anchor.Centre,
+            Origin = Anchor.Centre,
+            Depth = -1,
+            Font = FrameworkFont.Regular,
+            Colour = Color4.White
+        };
     }
 }
