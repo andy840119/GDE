0e6338e74ec56d8aa891321d19d2979def656fb0
AlFas <AlFasGD@users.noreply.github.com>
2019-07-29 04:21:30 +0300

    Add Player 1/2 Color properties in Color trigger

--- GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/Triggers/ColorTrigger.cs
+++ GDEdit/GDEdit/Utilities/Objects/GeometryDash/LevelObjects/Triggers/ColorTrigger.cs
@@ -63,7 +63,13 @@ namespace GDEdit.Utilities.Objects.GeometryDash.LevelObjects.Triggers
             get => opacity;
             set => opacity = (float)value;
         }
-        // IMPORTANT: The Player 1 and Player 2 properties are ignored because the Copied Color ID serves that purpose well
+        // IMPORTANT: The Player 1 and Player 2 setters are not implemented to avoid unnecessary assignments
+        /// <summary>The Player 1 Color property of the trigger.</summary>
+        [ObjectStringMappable(ObjectParameter.SetColorToPlayerColor1)]
+        public bool Player1Color => CopiedColorID == (int)SpecialColorID.P1;
+        /// <summary>The Player 2 Color property of the trigger.</summary>
+        [ObjectStringMappable(ObjectParameter.SetColorToPlayerColor2)]
+        public bool Player2Color => CopiedColorID == (int)SpecialColorID.P2;
         /// <summary>The copied Color ID of the trigger.</summary>
         [ObjectStringMappable(ObjectParameter.CopiedColorID)]
         public int CopiedColorID
