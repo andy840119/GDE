42ba9093eeee6363845853b80fb9b5cdef1676eb
AlFas <AlFasGD@users.noreply.github.com>
2019-08-04 01:49:54 +0300

    Improve documentation

--- GDEdit/GDE.App/Main/UI/GDEButton.cs
+++ GDEdit/GDE.App/Main/UI/GDEButton.cs
@@ -18,15 +18,6 @@ namespace GDE.App.Main.UI
             BackgroundColour = GDEColors.FromHex("303030");
         }
 
-        // Okay peppy we need to fucking talk like men
-        // Setting the color of the button's text to yellow by default on the framework is real mad shit
-        // This is pretty much a crime against fundamental framework rules
-        // While you wanted the text to appear yellow for whatever fucking reason (that color fucking sucks btw), you cannot personalize it on a framework level
-        // It makes the most sense for the default color to be white
-        // Since it is a neutral color and clashes with the background color which defaults to black IIRC
-        // Therefore you've done mad shit and you cannot be forgiven anymore
-        // Having to overload this function just because you decided to change the default color to something ugly is so bullshit
-        // Just make a fucking FrameworkButton and overload the function there for your beautiful yellow color ffs
         protected override SpriteText CreateText() => new SpriteText
         {
             Anchor = Anchor.Centre,
